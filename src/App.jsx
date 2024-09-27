8c9f8bd1e96316040d029eac03f4f8262d756936
Sancho <sanzarospanov@Sanchos-MacBook-Air.local>
2024-09-27 21:09:31 +0500

    add to app-component this year

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@
 import ReactLogo from './assets/react.svg?react'
 import './App.css'
 
+// declarative
+const thisYear = new Date().getFullYear()
+
+// imperative
 export const App = () => (
 	<div>
 		<h1>Hello World!</h1>
 		<ReactLogo />
+		<h2>{thisYear}</h2>
 	</div>
 )
 
